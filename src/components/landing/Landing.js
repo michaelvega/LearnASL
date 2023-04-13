face67b405fe4c4d7df9b3d68cb87cea195aa8f0
Michael Vega <michaelvega8888@gmail.com>
2023-04-13 15:28:27 -0400

    Add Cards Update Landing.js Update Notice.css Update Notice.js

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -5,7 +5,10 @@ import {Link, useNavigate} from "react-router-dom";
 import Card from "../card/Card";
 import "../card/Card.css"
 import { Button } from 'antd';
-
+import y from "./imgs/y-capture.png"
+import b from "./imgs/b-capture.png"
+import g from "./imgs/g-capture.png"
+import thankyou from "./imgs/thank-you-asl.gif"
 
 function Landing() {
 
@@ -24,7 +27,28 @@ function Landing() {
 
       </div>
       <p style={{margin: "1rem"}}>LearnASL.org gamifies the american sign language learning process, by giving helpful tutorials and real-time feedback.</p>
-
+      <div className="cards-container">
+        <Card
+          title="Helpful Markers"
+          image={y}
+          description="Utilizes AI algorithm to detect hand features (red dots)."
+        />
+        <Card
+          title="Real Time Feedback"
+          image={b}
+          description="Model object recognizes the user-selected word to practice."
+        />
+        <Card
+          title="Fingerspelling"
+          image={g}
+          description="Currently the alphabet is supported, enabling fingerspelling."
+        />
+        <Card
+          title="New Features"
+          image={thankyou}
+          description="More features including new words and phrases coming soon..."
+        />
+      </div>
     </div>
   );
 }
--- src/components/notice/Notice.js
+++ src/components/notice/Notice.js
@@ -1,24 +1,31 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./Notice.css"
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import b from "../landing/imgs/b-capture.png";
+import {Button} from "antd";
 
 
 function Notice() {
 
+  const navigate = useNavigate();
 
+  const navigateHome = () => {
+    navigate('/home');
+  };
 
   return (
     <div className={"textWrapperHorizontalNotice"}>
-
-      <h1 className="title" style={{marginBottom: "0.5rem"}}><b>This Site is still in Development/Prototype Stage &#129514; </b></h1>
-      <ul>
-        <li> <p>More words are being added currently. </p></li>
+      <h1 className="title"><center><b>This Site is still in Prototype Stage &#129514; </b></center></h1>
+      <ul className={"ulist"}>
+        <li> <p>More words are being added currently, as development continues.</p></li>
         <li> <p>Support for real time sign language detection over the web is also being worked on. </p></li>
         <li> <p>Our Alpha-2 computer vision algorithm has achieved near perfect accuracy for the sign alphabet on our testing data. </p></li>
       </ul>
 
-      <Link to="/home"> <h3>Click here to start learning... </h3> </Link>
+      <Button onClick={navigateHome} type="primary" size={"large"}>Click here to start Learning... 	&#10145;</Button>
+
+      <iframe className={"video-frame"} src="https://www.youtube.com/embed/s271vcQwJRc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   );
 }
