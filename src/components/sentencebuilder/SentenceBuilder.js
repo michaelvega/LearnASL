1a7303900d4bc630dac18fe6cd071d99ed1cc08c
Michael Vega <michaelvega8888@gmail.com>
2024-04-22 16:43:20 -0400

    learnasli2p

--- src/components/sentencebuilder/SentenceBuilder.js
+++ src/components/sentencebuilder/SentenceBuilder.js
@@ -1,120 +1,113 @@
-import React, {useEffect, useState} from 'react';
-import "./SentenceBuilder.css"
+import React, { useEffect, useState } from 'react';
+import "./SentenceBuilder.css";
 import 'bootstrap/dist/css/bootstrap.css';
-import {useParams, useNavigate} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import WordList from "../wordlist/WordList";
 import NotFound from "../notfound/NotFound";
-import {RightCircleFilled, LeftCircleFilled} from "@ant-design/icons";
+import { RightCircleFilled, LeftCircleFilled } from "@ant-design/icons";
 
 function SentenceBuilder() {
   const params = useParams();
-  // console.log("hi1");
   const navigate = useNavigate();
+  const [predictedLetter, setPredictedLetter] = useState('');
+
+  const wordID = params.wordID;
+
+  const prop = WordList.find((p) => p.id === parseInt(wordID));
+  const title = prop.title;
+  const propId = prop.id;
+  const singleLetter = prop.singleLetter;
+  const instructions = prop.instructions;
+  const image1 = prop.image1;
+  const image2 = prop.image2;
+
+
+  useEffect(() => {
+    if (wordID) {
+      const prop = WordList.find((p) => p.id === parseInt(wordID));
+      if (prop) {
+        const propId = prop.id;
+
+        console.log(prop.singleLetter);
+
+
+        // Set target letter on the Flask backend
+        fetch(`https://learnasl.ue.r.appspot.com/set_target_letter`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ letter: String(prop.singleLetter) })
+        })
+            .then(response => response.json())
+            .then(data => {
+              console.log('Target letter set:', data.message);
+              // After setting the target letter, retrieve the predicted letter
+              return fetch('https://learnasl.ue.r.appspot.com/get_predicted_letter');
+            })
+            .then(response => response.json())
+            .then(data => {
+              setPredictedLetter(data.predicted_letter);
+            })
+            .catch(error => console.error('Error:', error));
+      }
+    }
+  }, [params.wordID]);  // This effect will re-run when wordID changes
 
   try {
     const wordID = params.wordID;
-
     const prop = WordList.find((p) => p.id === parseInt(wordID));
-    const title = prop.title;
-    const propId = prop.id;
-    const singleLetter = prop.singleLetter;
-    const instructions = prop.instructions;
-    const image1 = prop.image1;
-    const image2 = prop.image2;
+    if (!prop) throw new Error("Word not found");
 
-    localStorage.setItem(String(propId), "completed");
-    localStorage.setItem("current", String(propId));
+    const {title, singleLetter, instructions} = prop;
 
     const navigateForward = () => {
-      let newUrl = `/practice/${String(parseInt(wordID) + 1)}`
+      let newUrl = `/practice/${String(parseInt(wordID) + 1)}`;
       if ((parseInt(wordID) + 1) > 26) {
         newUrl = "/practice/1";
       }
-      navigate(String(newUrl));
+      navigate(newUrl);
     };
+
     const navigateBackward = () => {
-      let newUrl = `/practice/${String(parseInt(wordID) - 1)}`
+      let newUrl = `/practice/${String(parseInt(wordID) - 1)}`;
       if ((parseInt(wordID) - 1) < 1) {
         newUrl = "/practice/26";
       }
-      navigate(String(newUrl));
+      navigate(newUrl);
     };
 
+    //<p>Predicted Letter: {predictedLetter}</p>
+
     return (
-      <div className={"sentenceBuilderWrapper"}>
-        <div className={"btnArea"}>
-          <button type="button" className="btn btn-link" onClick={navigateBackward} style={{padding: 0}}><p>Previous</p><LeftCircleFilled style={{ fontSize: '3em', color: 'dimgray' }} /></button>
-          <button type="button" className="btn btn-link" onClick={navigateForward} style={{padding: 0}}><p>Next</p><RightCircleFilled style={{ fontSize: '3em', color: 'dimgray' }} /></button>
-        </div>
-        <div className={"textWrapperHorizontalLearn"}>
-          <h1 style={{fontSize: "10em"}}>{singleLetter}</h1>
-          <div className="sentence">{title}</div>
+        <div className={"sentenceBuilderWrapper"}>
+          <div className={"btnArea"}>
+            <button type="button" className="btn btn-link" onClick={navigateBackward} style={{padding: 0}}>
+              <p>Previous</p><LeftCircleFilled style={{fontSize: '3em', color: 'dimgray'}}/></button>
+            <button type="button" className="btn btn-link" onClick={navigateForward} style={{padding: 0}}><p>Next</p>
+              <RightCircleFilled style={{fontSize: '3em', color: 'dimgray'}}/></button>
+          </div>
+          <div className={"textWrapperHorizontalLearn"}>
+            <h1 style={{fontSize: "10em"}}>{singleLetter}</h1>
+            <div className="sentence">{title}</div>
+            <div className={"contentArea"}>
+              <article>
+                <center>{instructions}</center>
+              </article>
+              <img src={image1} alt={"The ASL sign for " + title}></img>
+              <img src={image2} alt={"The ASL sign for " + title}></img>
 
-          <div className={"contentArea"}>
-
-            <article ><center>{instructions}</center></article>
-            <img src={image1} alt={"The ASL sign for " + title}></img>
-            <img src={image2} alt={"The ASL sign for " + title}></img>
+              <iframe width="640" height="480" src="https://learnasl.ue.r.appspot.com" frameBorder="0" allow="camera"
+                      allowFullScreen></iframe>
+            </div>
           </div>
         </div>
-      </div>
     );
 
   } catch (error) {
-    // console.log("error");
+    console.error("Rendering error:", error);
     return <NotFound />;
   }
-
-  /*
-  const words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-  const [filler, setFiller] = useState(true);
-
-  const [selectedWords, setSelectedWords] = useState([]);
-
-  function handleWordClick(word) {
-    setSelectedWords((prevWords) => [...prevWords, word]);
-    setFiller(false);
-  }
-
-  function handleClearClick() {
-    setSelectedWords([]);
-    setFiller(true);
-  }
-
-  function handleCheckClick() {
-    const selectedSentence = selectedWords.join(' ');
-    if (selectedSentence === sentence) {
-      alert('Correct!');
-    } else {
-      alert('Incorrect!');
-    }
-    setSelectedWords([]);
-  }
-  <div className="word-list">
-        {words.map((word) => (
-          <span key={word} className="word" onClick={() => handleWordClick(word)}>
-            {word}
-          </span>
-        ))}
-      </div>
-      <div className="selected-words">
-        {selectedWords.map((word) => (
-          <span key={word} className="selected-word">
-            {word}
-          </span>
-        ))}
-        <div>
-          <p style={{ display: filler ? "" : "none" }} className={"filler"}>Click on words to form sentence!</p>
-        </div>
-
-      </div>
-
-      <button className="btn btn-sm btn-primary" onClick={handleCheckClick}>Check</button>
-        <button className="btn btn-sm btn-primary" onClick={handleClearClick}>Clear</button>
-   */
-
-
 }
 
 export default SentenceBuilder;
