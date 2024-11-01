9e5f34364e655e376d4198be1cff2509a311ce2b
Andre Grossberg <andre.gro@gatech.edu>
2024-11-01 01:54:00 -0400

    Added Aloeha Animation

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -6,7 +6,7 @@ import { Button, Progress } from "antd";
 import Aloeha7 from "../../assets/Aloeha7.png"
 import Aloeha3 from "../../assets/Aloeha3.png"
 import Aloeha6 from "../../assets/Aloeha6.png"
-import Aloeha4 from "../../assets/Aloeha4.png"
+import Aloeha15 from "../../assets/Aloeha15.png"
 import Aloeha14 from "../../assets/Aloeha14.png"
 import textBubble from "../../assets/textBubble.png";
 
@@ -18,8 +18,8 @@ const frames = [
     { text: 'Hi! My name is Aloeha the Aloe Plant, and I will be your guide!', image: Aloeha7, insertName: false},
     { text: 'Welcome to LearnASL!', image: Aloeha3, insertName: false},
     { text: 'Lets get you right into it so you can really see what LearnASL is all about!', image: Aloeha6, insertName: false},
-    { text: 'Please start by entering your preferred first name in the text box below', image: Aloeha4, insertName: true},
-    { text: 'Sweet! Also, did you notice the progress bar move? This will show you how far in a lesson you are. Awesome first step.', image: Aloeha14, insertName: false},
+    { text: 'Please start by entering your preferred first name in the text box below.', image: Aloeha15, insertName: true},
+    { text: 'Sweet! Also did you notice the progress bar move? This will show you how far in a lesson you are. Awesome first step.', image: Aloeha14, insertName: false},
     { text: 'To get you situated with how we work, lets teach you how to introduce yourself in ASL!', image: Aloeha6, insertName: false}
 ];
 
